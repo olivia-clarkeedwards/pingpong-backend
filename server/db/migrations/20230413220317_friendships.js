3656ae002341656965fdf6a6cb9d42ab153104ca
J-Haynes <114152650+J-Haynes@users.noreply.github.com>
2023-04-14 10:05:18 +1200

    friendships migration

---
+++ server/db/migrations/20230413220317_friendships.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('friendships', (table) => {
+    table.string('user_one_id')
+    table.string('user_two_id')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('friendships')
+}
