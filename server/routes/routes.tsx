619e26023019082f568d6e16da3e46cf85831ee7
Ryan-Kendrick <118949957+Ryan-Kendrick@users.noreply.github.com>
2023-04-14 15:03:37 +1200

    WIP

--- server/routes/routes.tsx
+++ server/routes/routes.tsx
@@ -1,29 +1,43 @@
 import express from 'express'
 import { User, UserWithFriends } from '../../common/interface'
-import { confirmFriendRequest } from '../db/dbFuncs'
-import { getFriends, addFriend } from '../db/dbUtils'
+import { confirmFriendRequest, setPing } from '../db/dbFuncs'
+import { getFriends } from '../db/dbUtils'
 const router = express.Router()
 
 router.use(express.json())
 
-router.get('/', (req, res) => {
-  const id = 'google-oauth|123456789101'
-  return getFriends(id).then((friends: UserWithFriends) => res.json(friends))
+// Takes a userId and returns that user and an array of friends
+router.post('/getfriends', (req, res) => {
+  const userId = req.body.userId
+  return getFriends(userId).then((friends: UserWithFriends) =>
+    res.json(friends)
+  )
 })
 
-router.post('/', (req, res) => {
-  const friend = req.body
-  return addFriend(friend).then((addedFriend) => {
-    res.json(addedFriend)
-  })
-})
+// add user
+// router.post('/', (req, res) => {
+//   const friend = req.body
+//   return addFriend(friend).then((addedFriend) => {
+//     res.json(addedFriend)
+//   })
+// })
 
+// Takes a userId and a friendId and returns 1 if they are made friends by setting pending to false in the db
 router.post('/confirm', (req, res) => {
-  const user = req.body.user
-  const friend = req.body.friend
-  return confirmFriendRequest(user, friend).then((response) => {
+  const user = req.body.userId
+  const friend = req.body.friendId
+  return confirmFriendRequest(user, friend).then((response) =>
     res.json(response)
-  })
+  )
 })
+
+// set ping
+// router.post('/toggleping', (req, res) => {
+//   const id = req.body.userId
+//   return setPing(id).then((response) => res.json(response))
+// })
+
+// return user by auth0
+// add friend request
 
 export default router
--- server/server.ts
+++ server/server.ts
@@ -9,4 +9,4 @@ server.use(express.static(path.join(__dirname, 'public')))
 
 export default server
 
-server.use('/api/v1/routes', routes)
+server.use('/api/v1/', routes)
