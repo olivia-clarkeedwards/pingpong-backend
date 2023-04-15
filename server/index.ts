eabdba58ea9e68b35ab5748eae141ed8a47b2233
Ryan-Kendrick <118949957+Ryan-Kendrick@users.noreply.github.com>
2023-04-15 17:12:52 +1200

    add cors

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,13 @@ import server from './server'
 
 const PORT = process.env.PORT || 3000
 
+const cors = require('cors')
+server.use(
+  cors({
+    origin: '*',
+  })
+)
+
 server.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('Listening on port', PORT)
