bfc249adb2d3c922d9126fe5327b5e440e240079
Ryan-Kendrick <118949957+Ryan-Kendrick@users.noreply.github.com>
2023-04-15 17:23:54 +1200

    cors

--- server/index.ts
+++ server/index.ts
@@ -3,11 +3,7 @@ import server from './server'
 const PORT = process.env.PORT || 3000
 
 const cors = require('cors')
-server.use(
-  cors({
-    origin: '*',
-  })
-)
+server.use(cors())
 
 server.listen(PORT, () => {
   // eslint-disable-next-line no-console
