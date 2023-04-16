48acb68ae15bbf37cae28cec32693070c4012a87
Olivia Grace <121794969+olivia-clarkeedwards@users.noreply.github.com>
2023-04-16 14:39:35 +1200

    add route to get place details by id

--- server/routes/googleApiRoutes.ts
+++ server/routes/googleApiRoutes.ts
@@ -6,9 +6,22 @@ const GOOGLE_API_KEY = 'AIzaSyBxY5MSXvEFGxD-HBKHVdCl1Xb4x8HABY0'
 const router = express.Router()
 router.use(express.json())
 
+//gets locations from google places for autofill component
 router.get('/place/autocomplete/json?', (req, res) => {
   const { input } = req.query
   const url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${input}&key=${GOOGLE_API_KEY}&language=en`
+  return request
+    .get(url)
+    .then((response) => {
+      return res.json(response.body)
+    })
+    .catch((err) => console.log(err.message))
+})
+
+//gets details by place id to select option for autofill component
+router.get('/place/details/json?', (req, res) => {
+  const { placeid } = req.query
+  const url = `https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeid}&key=${GOOGLE_API_KEY}&language=en`
   return request
     .get(url)
     .then((response) => {
