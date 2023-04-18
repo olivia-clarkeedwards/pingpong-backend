493994a87c36939201f02e7666f69f2c923ee5f1
Olivia Grace <121794969+olivia-clarkeedwards@users.noreply.github.com>
2023-04-19 10:19:41 +1200

    configure test db, run migrations and seeds before testing

--- server/db/dbFuncs.test.ts
+++ server/db/dbFuncs.test.ts
@@ -1,1 +1,12 @@
 import connection from './connection'
+import * as db from './dbFuncs'
+
+const testDb = connection
+
+beforeAll(async () => {
+  await testDb.migrate.latest()
+})
+
+beforeAll(async () => {
+  await testDb.seed.run()
+})
